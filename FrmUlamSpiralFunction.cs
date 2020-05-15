4eb7a2e5c0e67327e4460be92e66d39f3a1f4ac2
Joseph Fischetti <fischettijw@gmail.com>
2020-05-15 09:32:23 -0400

    I think I'm DONE

--- FrmUlamSpiralFunction.cs
+++ FrmUlamSpiralFunction.cs
@@ -20,13 +20,28 @@ namespace cSharpUlamSpiralFunction
 
         private void FrmUlamSpiralFunction_Load(object sender, EventArgs e)
         {
-            int[] n = new int[13] { 360, 364, 1000, 25, 73, 4, 8, 2, 6, 10, 3, 7, 2381 };
+            int[] n = new int[14] { 360, 364, 1000, 25, 73, 4, 8, 2, 6, 10, 3, 7, 2381, 1000 };
+            int[] x = new int[14] { 9, 6, -16, 2, 4, -1, 1, -1, 1, 0, -1, 1, 24,-16 };
+            int[] y = new int[14] { 9, 10, 8, 3, -3, -1, 1, 1, -1, 2, 0, 0, 5 , -8};
+
+            string checkOK;
 
             for (int i = 0; i < n.Length; i++)
             {
                 Point coord = UlamSpiralCoordinates(n[i]);
-            //    LbxOutput.Items.Add($"n={n[i]}  x={coord.X}    y={coord.Y}");
-                LbxOutput.Items.Add($" {i.RJ(3, '0')}   n={n[i].RJ(6, ' ', 5)} x={coord.X.RJ(4, ' ', 5)} y={coord.Y.RJ(4, ' ')}");
+                //    LbxOutput.Items.Add($"n={n[i]}  x={coord.X}    y={coord.Y}");
+
+                if (x[i]== coord.X && y[i]== coord.Y)
+                {
+                    checkOK = "OK";
+                }
+                else
+                {
+                    checkOK = $"Error:  {x[i]}, {y[i]} ";
+                }
+
+
+                LbxOutput.Items.Add($" {i.RJ(3, '0')}   n={n[i].RJ(6, ' ', 5)} x={coord.X.RJ(4, ' ', 5)} y={coord.Y.RJ(4, ' ')}    {checkOK}   ");
             }
         }
 
